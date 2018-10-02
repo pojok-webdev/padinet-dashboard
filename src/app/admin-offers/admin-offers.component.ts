a904c21ce681d9abfe56a0325742c86ebb46c484
pujie <puji@gmail.com>
2018-10-02 16:54:23 +0700

    add offers

--- src/app/admin-offers/admin-offers.component.ts
+++ src/app/admin-offers/admin-offers.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SalesService } from '../sales.service';
+import { EdgeInfoComponent } from '../edge-info/edge-info.component';
+import { MatTableDataSource, MatDialog } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
+import { PadiCytoscape } from 'padicytoscape';
+import { EdgeService } from '../edge.service';
 
 @Component({
   selector: 'app-admin-offers',
@@ -7,21 +12,54 @@ import { SalesService } from '../sales.service';
   styleUrls: ['./admin-offers.component.css']
 })
 export class AdminOffersComponent implements OnInit {
-  offer = {
-    clientname:'',
-    address:'',
-    pic:'',
-    phone:'',
-    purpose:''
-  }
+  dataSource : MatTableDataSource<any>
+  columnDisplayed = ["kdoffer","clientname","address","pic","email","phone","service","action"]
+  searchBox = ""
+  
   constructor(
-    private salesservice : SalesService
-  ) { }
-
+    private salesservice: SalesService,
+    private edges : EdgeService,
+    private dialog : MatDialog,
+    private cy : PadiCytoscape,
+    private route : ActivatedRoute,
+  ) {
+    let config = this.route.routeConfig
+    let params = this.route.snapshot.params
+    console.log("COnfig",config)
+    this.salesservice.getOffers(result => {
+      this.dataSource = new MatTableDataSource(result)
+    })
+  }
+  applyFilter(filterValue: string) {
+    console.log("filtervl",filterValue)
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
   ngOnInit() {
   }
-  save(offer){
-    this.salesservice.saveOffer(offer, result => {})
+  editEdge(edge){
+    edge.source = edge.source.toString()
+    edge.target = edge.target.toString()
+    console.log("Edge",edge)
+    this.dialog.open(EdgeInfoComponent,{
+      width: '500px',
+      data:{
+        component:edge
+      }
+    })
   }
+  removeEdge(element){
+    console.log("Elemeent",element)
+    this.edges.removeEdge(element,result => {
+      this.cy.removeEdge(element.id)
+      console.log("Result",result)
+    })
+  }
+  clearSearchbox(){
+    this.searchBox = ""
+    this.applyFilter("")
+  }
+
 
 }
